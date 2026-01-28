8216245f5a5e876664c7413bb9020a6c6da97fdf
utkqrshsingh <utkqrshsingh@gmail.com>
2026-01-29 00:21:23 +0530

    nicks and changes to gradlebuild:app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.mobile.habittrackernew"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
